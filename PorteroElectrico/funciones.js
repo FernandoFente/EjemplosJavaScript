fac4abc8d846003f1e9a0e1fa39907921160e5d3
Fernando Fente <fernando.fente@outlook.es>
2020-10-09 19:36:29 -0300

    portero electrico

---
+++ PorteroElectrico/funciones.js
@@ -0,0 +1,10 @@
+function borrar(){
+    document.getElementById("depto").value = "";
+    document.getElementById("piso").value = "";
+}
+
+function rellenar(num){
+    document.getElementById("piso").value = num;
+}
+
+
