511d4b61ac1bf2eb1cb1ea73c19740e7d20e541c
Fernando Fente <fernando.fente@outlook.es>
2020-10-10 14:56:11 -0300

    creo funcion llamar, error 'debug'

--- PorteroElectrico/funciones.js
+++ PorteroElectrico/funciones.js
@@ -3,8 +3,24 @@ function borrar(){
     document.getElementById("piso").value = "";
 }
 
+function llamar(depto,piso){
+    depto = document.getElementById("depto").value;
+    piso = document.getElementById("piso").value;
+    alert(piso);
+    alert(depto);
+    let mensaje = "llamando a piso "+piso+" departamento "+depto;
+    alert(mensaje);
+    if(piso<=48 && depto<=9){
+        document.getElementById("llamando").value = mensaje;
+    }
+}
+
 function rellenar(num){
-    document.getElementById("piso").value = num;
+    let piso = document.getElementById("piso").value = num + num;
+    if(piso > 49){
+        alert("el piso no existe");
+        document.getElementById("llamando").value = "el piso " +piso+" no existe";
+    }
 }
 
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # EjemplosJava
 Repositorio para subir ejercicios de javaScript
+**Me salta un error que me lo guarda en un txt en mi carpeta ./porteroElectrico/debug**
