aa9f8fccc84c83f50fce00eff18dc90e5a1bc4f8
Fernando Fente <fernando.fente@outlook.es>
2020-10-10 15:05:56 -0300

    cambio en funciones js

--- PorteroElectrico/funciones.js
+++ PorteroElectrico/funciones.js
@@ -6,21 +6,20 @@ function borrar(){
 function llamar(depto,piso){
     depto = document.getElementById("depto").value;
     piso = document.getElementById("piso").value;
-    alert(piso);
-    alert(depto);
+    /*alert(piso);
+    alert(depto);*/
     let mensaje = "llamando a piso "+piso+" departamento "+depto;
-    alert(mensaje);
+    /*alert(mensaje);*/
     if(piso<=48 && depto<=9){
         document.getElementById("llamando").value = mensaje;
+    }else if(piso > 49){
+        alert("el piso no existe");
+        document.getElementById("llamando").value = "el piso " +piso+" no existe";
     }
 }
 
 function rellenar(num){
-    let piso = document.getElementById("piso").value = num + num;
-    if(piso > 49){
-        alert("el piso no existe");
-        document.getElementById("llamando").value = "el piso " +piso+" no existe";
-    }
+    document.getElementById("piso").value = num + num;
 }
 
 
