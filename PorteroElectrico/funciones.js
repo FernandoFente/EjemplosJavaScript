efbfebbb44554ee6310ed963bc7d7dd4964f9d15
Fernando Fente <fernando.fente@outlook.es>
2020-10-11 19:10:14 -0300

    Ultimos detalles del portero electrico

--- PorteroElectrico/funciones.js
+++ PorteroElectrico/funciones.js
@@ -1,6 +1,9 @@
 function borrar(){
     document.getElementById("depto").value = "";
     document.getElementById("piso").value = "";
+    document.getElementById("llamando").value = "";
+    let audio = new Audio("beep_short.ogg");
+    audio.play();
 }
 
 function llamar(depto,piso){
@@ -10,16 +13,26 @@ function llamar(depto,piso){
     alert(depto);*/
     let mensaje = "llamando a piso "+piso+" departamento "+depto;
     /*alert(mensaje);*/
-    if(piso<=48 && depto<=9){
+    if(piso > 48){
+        document.getElementById("llamando").value = "el piso "+piso+" no existe (01 - 48)";
+        document.getElementById("llamando").style.color = "red";
+    }else if( depto > 6){
+        document.getElementById("llamando").value = "el depto " +depto+" no existe (1 - 6)";
+        document.getElementById("llamando").style.color = "red";
+    } else {
         document.getElementById("llamando").value = mensaje;
-    }else if(piso > 49){
-        alert("el piso no existe");
-        document.getElementById("llamando").value = "el piso " +piso+" no existe";
+        document.getElementById("llamando").style.color = "green";
     }
 }
 
 function rellenar(num){
-    document.getElementById("piso").value = num + num;
+    let valor = document.getElementById("piso").value + num;
+    if(valor.length <= 2){
+        document.getElementById("piso").value = valor;
+    } else{
+        document.getElementById("depto").value = num;
+    }
+
+    let audio = new Audio("beep_short.ogg");
+    audio.play();
 }
-
-
