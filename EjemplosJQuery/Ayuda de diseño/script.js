03477f0d450ea59d3212c218a02e10b838aa1a9d
Fernando Fente <fernando.fente@outlook.es>
2020-10-26 17:19:29 -0300

    agrego asesor de diseño y otros cambios

---
+++ EjemplosJQuery/Ayuda de diseño/script.js
@@ -0,0 +1,84 @@
+/*Colores de Fuente */
+
+$(document).ready(function(){
+    $(".text-primary").click(function(){
+        $("#titulos").css("color", "#007bff")
+    })
+})
+
+$(".text-primary").click(function(){
+    $("#titulos").css("color", "#007bff")
+})
+
+$(".text-secondary").click(function(){
+    $("#titulos").css("color", "#6c757d")
+})
+
+$(".text-warning").click(function(){
+    $("#titulos").css("color", "#ffc107")
+})
+
+$(".text-success").click(function(){
+    $("#titulos").css("color", "#28a745")
+})
+
+$(".text-danger").click(function(){
+    $("#titulos").css("color", "#dc3545")
+})
+
+$(".text-light").click(function(){
+    $("#titulos").css("color", "#f8f9fa")
+})
+
+$(".text-dark").click(function(){
+    $("#titulos").css("color", "#343a40")
+})
+
+/*Tipos de Fuente */
+
+$("#syne").click(function(){
+    $("#titulos").css("font-family","Syne Tactile")
+})
+
+$("#montserrat").click(function(){
+    $("#titulos").css("font-family","Montserrat")
+})
+
+$("#roboto").click(function(){
+    $("#titulos").css("font-family","roboto")
+})
+
+$("#turret").click(function(){
+    $("#titulos").css("font-family","Turret Road")
+})
+
+/*Color de fondo */
+
+$(".bg-primary").click(function(){
+    $("#titulos").css("background-color","#007bff")
+})
+
+$(".bg-secondary").click(function(){
+    $("#titulos").css("background-color","#6c757d")
+})
+
+$(".bg-warning").click(function(){
+    $("#titulos").css("background-color","#ffc107")
+})
+
+$(".bg-success").click(function(){
+    $("#titulos").css("background-color","#28a745")
+})
+
+$(".bg-danger").click(function(){
+    $("#titulos").css("background-color","#dc3545")
+})
+
+$(".bg-light").click(function(){
+    $("#titulos").css("background-color","#f8f9fa")
+})
+
+$(".bg-dark").click(function(){
+    $("#titulos").css("background-color","#343a40")
+})
+
